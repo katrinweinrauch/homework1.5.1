edc65e16d3e549f844f99ec98f896fcac8a80b61
katrinweinrauch <katrinweinrauch1@yandex.ru>
2024-08-06 18:07:02 +0500

    Add files via upload

---
+++ неизм+кортежи.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2, 3, True, 'Love', '1'
+print(immutable_var)
+print(type(immutable_var))
+print(immutable_var[0])
+immutable_var[0]
+mutable_list = [1, 2, 3, True, 'Love', '1']
+print(type(mutable_list))
+mutable_list[2] = 7
+print(mutable_list)
